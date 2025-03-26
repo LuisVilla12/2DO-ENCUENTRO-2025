07c5404bb6e3f436b773acbb47ab614cb5aec719
Luis Villa <luisji2312ia@gmail.com>
2025-03-26 15:40:38 -0600

    Version Casi final 8

--- confirmar_asistencia.php
+++ confirmar_asistencia.php
@@ -1,7 +1,7 @@
 <?php
 // Configuración de la conexión a la base de datos
 $servername = "localhost";  // O la IP del servidor si no es localhost
-$username = "luisvilla";     // Nombre de usuario de la base de datos
+$username = "luivilla";     // Nombre de usuario de la base de datos
 $password = "lkqaz923";   // Contraseña de la base de datos
 $dbname = "encuentroca";   // Nombre de la base de datos
 $port = 3306;  // Puerto personalizado (en tu caso, es el 330)
@@ -17,16 +17,18 @@ if ($conn->connect_error) {
 // Verificar si los datos han sido enviados por POST
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Recoger los datos del formulario
-    $name = $_POST['name'];
-    $lastname_p = $_POST['lastname_p'];
-    $lastname_m = $_POST['lastname_m'];
-    $email = $_POST['email'];
-    $institucion = $_POST['institucion'];
-    $telefono = $_POST['telefono'];
+    $confirmar_asistencia = $_POST['confirmar_asistencia'];
+    $institucion =  $_POST['institucion'];
     $ca_nombre = $_POST['ca_nombre'];
     $ca_clave = $_POST['ca_clave'];
+    $autores = $_POST['autores'];
+    $grado_consolidacion = $_POST['grado_consolidacion'];
+    $mesa = $_POST['mesa'];
+    $modalidad = $_POST['modalidad'];
+    $modo_informacion = $_POST['modo_informacion'];
+
     // Insertar los datos en la base de datos
-    $sql = "INSERT INTO registro_confirmacion (name_,lastname_p,lastname_m,email,institucion,telefono,ca_nombre,ca_clave) VALUES ('$name','$lastname_p','$lastname_m','$email','$institucion', '$telefono','$ca_nombre','$ca_clave')";
+    $sql = "INSERT INTO registro_asistencias (confirmar_asistencia, institucion, ca_nombre,ca_clave,autores,grado_consolidacion,mesa,modalidad,modo_informacion) VALUES ('$confirmar_asistencia', '$institucion', '$ca_nombre','$ca_clave','$autores','$grado_consolidacion','$mesa','$modalidad','$modo_informacion')";
 
     if ($conn->query($sql) === TRUE) {
         header("Location: gracias_por_registrarte.html");
