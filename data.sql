29e30c256057b5e1a97ff881714518d4b01abb7a
LuisVilla12 <luisji2312ia@gmail.com>
2025-09-27 12:36:59 -0600

    Constancias Add

---
+++ data.sql
@@ -0,0 +1,96 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+INSERT INTO `registro_constancias` (`id`, `tipo_constancia`, `path_constancia`, `name_constancia`) VALUES
+(1031, 'Logistica', 'Logistica_HectorRamirezHuerta.pdf\r', 'Hector Ramirez Huerta.pdf\r'),
+(1032, 'Logistica', 'Logistica_LuisAntonioVillegasQuiroz.pdf\r', 'Luis Antonio Villegas Quiroz.pdf\r'),
+(1033, 'Logistica', 'Logistica_XimenaFranciscoMartinez.pdf\r', 'Ximena Francisco Martinez.pdf\r'),
+(1034, 'Logistica', 'Logistica_AdrianOrellanTeyssier.pdf\r', 'Adrian Orellan Teyssier.pdf\r'),
+(1035, 'Logistica', 'Logistica_CarlosAlbertoMayZarinana.pdf\r', 'Carlos Alberto May Zarinana.pdf\r'),
+(1036, 'Logistica', 'Logistica_AxelMichelleCastroDiaz.pdf\r', 'Axel Michelle Castro Diaz.pdf\r'),
+(1037, 'Moderadores', 'Moderadores_MariadelRefugioCastanedaChavez.pdf\r', 'Maria del Refugio Castaneda Chavez.pdf\r'),
+(1038, 'Moderadores', 'Moderadores_FabiolaLangoReynoso.pdf\r', 'Fabiola Lango Reynoso.pdf\r'),
+(1039, 'Moderadores', 'Moderadores_JuanCarlosMorenoSecena.pdf\r', 'Juan Carlos Moreno Secena.pdf\r'),
+(1040, 'Moderadores', 'Moderadores_DulceMariaMartinezAngeles.pdf\r', 'Dulce Maria Martinez Angeles.pdf\r'),
+(1041, 'Moderadores', 'Moderadores_SagrarioAlejandreApolinar.pdf\r', 'Sagrario Alejandre Apolinar.pdf\r'),
+(1042, 'Moderadores', 'Moderadores_LiliaLiceaHernandez.pdf\r', 'Lilia Licea Hernandez.pdf\r'),
+(1043, 'Moderadores', 'Moderadores_VirginiaLagunesBarradas.pdf\r', 'Virginia Lagunes Barradas.pdf\r'),
+(1044, 'Moderadores', 'Moderadores_JoseArmandoLozadaGarcia.pdf\r', 'Jose Armando Lozada Garcia.pdf\r'),
+(1045, 'Organizadores', 'Organizadores_EliseoEscobedoMota.pdf\r', 'Eliseo Escobedo Mota.pdf\r'),
+(1046, 'Organizadores', 'Organizadores_MarthaJudithHernandezVelasco.pdf\r', 'Martha Judith Hernandez Velasco.pdf\r'),
+(1047, 'Organizadores', 'Organizadores_AdrianaDominguezReyes.pdf\r', 'Adriana Dominguez Reyes.pdf\r'),
+(1048, 'Organizadores', 'Organizadores_MariaFernandaGonzalezGonzalez.pdf\r', 'Maria Fernanda Gonzalez Gonzalez.pdf\r'),
+(1049, 'Organizadores', 'Organizadores_MariaSalomeAlejandreApolinar.pdf\r', 'Maria Salome Alejandre Apolinar.pdf\r'),
+(1050, 'Organizadores', 'Organizadores_HugoAmoresPerez.pdf\r', 'Hugo Amores Perez.pdf\r'),
+(1051, 'Organizadores', 'Organizadores_IrmaAngelicaGarciaGonzalez.pdf\r', 'Irma Angelica Garcia Gonzalez.pdf\r'),
+(1052, 'Organizadores', 'Organizadores_JuanCarlosMorenoSecena.pdf\r', 'Juan Carlos Moreno Secena.pdf\r'),
+(1053, 'Organizadores', 'Organizadores_JulioCesarCastanedaOrtega.pdf\r', 'Julio Cesar Castaneda Ortega.pdf\r'),
+(1054, 'Organizadores', 'Organizadores_OscarCarmonaHernandez.pdf\r', 'Oscar Carmona Hernandez.pdf\r'),
+(1055, 'Organizadores', 'Organizadores_YadeneyroDelaCruzElizondo.pdf\r', 'Yadeneyro De la Cruz Elizondo.pdf\r'),
+(1056, 'Organizadores', 'Organizadores_BenitoHernandezCastellanos.pdf\r', 'Benito Hernandez Castellanos.pdf\r'),
+(1057, 'Organizadores', 'Organizadores_JoseArmandoLozadaGarcia.pdf\r', 'Jose Armando Lozada Garcia.pdf\r'),
+(1058, 'Organizadores', 'Organizadores_JuanAntonioFranciscoGutierrez.pdf\r', 'Juan Antonio Francisco Gutierrez.pdf\r'),
+(1059, 'Organizadores', 'Organizadores_FabiolaLangoReynoso.pdf\r', 'Fabiola Lango Reynoso.pdf\r'),
+(1060, 'Organizadores', 'Organizadores_MariadelRefugioCastanedaChavez.pdf\r', 'Maria del Refugio Castaneda Chavez.pdf\r'),
+(1061, 'Organizadores', 'Organizadores_IsabelAraceliAmaroEspejo.pdf\r', 'Isabel Araceli Amaro Espejo.pdf\r'),
+(1062, 'Organizadores', 'Organizadores_JesusMontoyaMendoza.pdf\r', 'Jesus Montoya Mendoza.pdf\r'),
+(1063, 'Organizadores', 'Organizadores_JuanValenteMegchunGarcia.pdf\r', 'Juan Valente Megchun Garcia.pdf\r'),
+(1064, 'Organizadores', 'Organizadores_AdrianGutierrezCervantes.pdf\r', 'Adrian Gutierrez Cervantes.pdf\r'),
+(1065, 'Organizadores', 'Organizadores_MagnoliaGricelSalcedoGarduno.pdf\r', 'Magnolia Gricel Salcedo Garduno.pdf\r'),
+(1066, 'Organizadores', 'Organizadores_DaliaGuadalupeMendozaLopez.pdf\r', 'Dalia Guadalupe Mendoza Lopez.pdf\r'),
+(1067, 'Organizadores', 'Organizadores_ChristianReyesVelazquez.pdf\r', 'Christian Reyes Velazquez.pdf\r'),
+(1068, 'Organizadores', 'Organizadores_MariaGracielaHernandezyOrduna.pdf\r', 'Maria Graciela Hernandez y Orduna.pdf\r'),
+(1069, 'Organizadores', 'Organizadores_JessicaGeraldineVillatoroHernandez.pdf\r', 'Jessica Geraldine Villatoro Hernandez.pdf\r'),
+(1070, 'Organizadores', 'Organizadores_CeliaGabrielaSierraCarmona.pdf\r', 'Celia Gabriela Sierra Carmona.pdf\r'),
+(1071, 'Ponencia', 'Ponencia_JoseDanielJuarezBarajas.pdf\r', 'Jose Daniel Juarez Barajas.pdf\r'),
+(1072, 'Ponencia', 'Ponencia_BenignoOrtizMuniz.pdf\r', 'Benigno Ortiz Muniz.pdf\r'),
+(1073, 'Prototipo', 'Prototipo_EnriqueFranciscoCabJimenez.pdf\r', 'Enrique Francisco Cab Jimenez.pdf\r'),
+(1074, 'Prototipo', 'Prototipo_FranciscoViverosTorres.pdf\r', 'Francisco Viveros Torres.pdf\r'),
+(1075, 'Prototipo', 'Prototipo_JosimarMunozDelgado.pdf\r', 'Josimar Munoz Delgado.pdf\r'),
+(1076, 'Prototipo', 'Prototipo_IsraelRoblesHernandez.pdf\r', 'Israel Robles Hernandez.pdf\r'),
+(1077, 'Prototipo', 'Prototipo_JanethRamosLopez.pdf\r', 'Janeth Ramos Lopez.pdf\r'),
+(1078, 'Prototipo', 'Prototipo_LissethRiveraAguirre.pdf\r', 'Lisseth Rivera Aguirre.pdf\r'),
+(1079, 'Prototipo', 'Prototipo_ErikaAguilarAcosta.pdf\r', 'Erika Aguilar Acosta.pdf\r'),
+(1080, 'Prototipo', 'Prototipo_JoseAntonioQuirozQuiroz.pdf\r', 'Jose Antonio Quiroz Quiroz.pdf\r'),
+(1081, 'Prototipo', 'Prototipo_AnellAguilarAguilar.pdf\r', 'Anell Aguilar Aguilar.pdf\r'),
+(1082, 'Prototipo', 'Prototipo_HalleyGuadalupeGarciaGana.pdf\r', 'Halley Guadalupe Garcia Gana.pdf\r'),
+(1083, 'Prototipo', 'Prototipo_GuillermoGaonaJulian.pdf\r', 'Guillermo Gaona Julian.pdf\r'),
+(1084, 'Prototipo', 'Prototipo_CarlosGarciaSandoval.pdf\r', 'Carlos Garcia Sandoval.pdf\r'),
+(1085, 'Prototipo', 'Prototipo_LotharHernandezGaona.pdf\r', 'Lothar Hernandez Gaona.pdf\r'),
+(1086, 'Prototipo', 'Prototipo_JoseAlfredoBautistaRojas.pdf\r', 'Jose Alfredo Bautista Rojas.pdf\r'),
+(1087, 'Prototipo', 'Prototipo_AlanSalvadorLibrerosHerrera.pdf\r', 'Alan Salvador Libreros Herrera.pdf\r'),
+(1088, 'Prototipo', 'Prototipo_DiegoFaridMunizRamos.pdf\r', 'Diego Farid Muniz Ramos.pdf\r'),
+(1089, 'Prototipo', 'Prototipo_JesusOrtegaIslas.pdf\r', 'Jesus Ortega Islas.pdf\r'),
+(1090, 'Prototipo', 'Prototipo_AlexEduardoArellanoPolanco.pdf\r', 'Alex Eduardo Arellano Polanco.pdf\r'),
+(1091, 'Relator', 'Relator_JesusMontoyaMendoza.pdf\r', 'Jesus Montoya Mendoza.pdf\r'),
+(1092, 'Relator', 'Relator_JuanAntonioFranciscoGutierrez.pdf\r', 'Juan Antonio Francisco Gutierrez.pdf\r'),
+(1093, 'Relator', 'Relator_JuanValenteMegchunGarcia.pdf\r', 'Juan Valente Megchun Garcia.pdf\r'),
+(1094, 'Relator', 'Relator_AdrianGutierrezCervantes.pdf\r', 'Adrian Gutierrez Cervantes.pdf\r'),
+(1095, 'Relator', 'Relator_BenitoHernandezCastellanos.pdf\r', 'Benito Hernandez Castellanos.pdf\r'),
+(1096, 'RevisoresCartel', 'RevisoresCartel_BenitoHernandezCastellanos.pdf\r', 'Benito Hernandez Castellanos.pdf\r'),
+(1097, 'RevisoresCartel', 'RevisoresCartel_JoseJuanHuescaLopez.pdf\r', 'Jose Juan Huesca Lopez.pdf\r'),
+(1098, 'RevisoresCartel', 'RevisoresCartel_DaliaGuadalupeMendozaLopez.pdf\r', 'Dalia Guadalupe Mendoza Lopez.pdf\r'),
+(1099, 'RevisoresCartel', 'RevisoresCartel_JuanValenteMegchunGarcia.pdf\r', 'Juan Valente Megchun Garcia.pdf\r'),
+(1100, 'RevisoresCartel', 'RevisoresCartel_MagnoliaGricelSalcedoGarduno.pdf\r', 'Magnolia Gricel Salcedo Garduno.pdf\r'),
+(1101, 'RevisoresCartel', 'RevisoresCartel_OscarCarmonaHernandez.pdf\r', 'Oscar Carmona Hernandez.pdf\r'),
+(1102, 'RevisoresCartel', 'RevisoresCartel_MariaGuadalupeGutierrezQuevedo.pdf\r', 'Maria Guadalupe Gutierrez Quevedo.pdf\r'),
+(1103, 'RevisoresCartel', 'RevisoresCartel_JulioCesarCastanedaOrtega.pdf\r', 'Julio Cesar Castaneda Ortega.pdf\r'),
+(1104, 'RevisoresCartel', 'RevisoresCartel_JuanAntonioFranciscoGutierrez.pdf\r', 'Juan Antonio Francisco Gutierrez.pdf\r'),
+(1105, 'RevisoresCartel', 'RevisoresCartel_MiguelAngelDiazArredondo.pdf\r', 'Miguel Angel Diaz Arredondo.pdf\r'),
+(1106, 'RevisoresCartel', 'RevisoresCartel_ChristianReyesVelazquez.pdf\r', 'Christian Reyes Velazquez.pdf\r'),
+(1107, 'RevisoresCartel', 'RevisoresCartel_EdnaF.CastilloMarquez.pdf\r', 'Edna F. Castillo Marquez.pdf\r');
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
